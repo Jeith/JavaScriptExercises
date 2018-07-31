cc87dd312b57f3e835262ce99c41b0eb18ea7fdf
Jeith <keithp1208@yahoo.com>
2018-07-31 13:16:28 -0500

    finished game-of-thrones api practice

--- gameofthrones/main.js
+++ gameofthrones/main.js
@@ -1,8 +1,10 @@
 (function() {  
 
+    for (var j = 1; j < 23; j++){
+    var url = "https://www.anapioficeandfire.com/api/characters?page=" + j + "&pageSize=50";
 
-    var url = "https://www.anapioficeandfire.com/api/characters?page=1&pageSize=50";
     $.get(url)
+
     .done(function(response) {
 
 		console.log(response);
@@ -18,23 +20,30 @@
 
 	//handle XHR success
 	function updateUISuccess(response) {
-
         for (var i = 0; i < 25; i++){
             var charlist = response[i];
             if (charlist.name != "" || charlist.name != undefined){
                 var $leftdiv = $('#leftdiv');
-                $leftdiv.append("<li><a>" + charlist.name + '</a></li>');
+                if (charlist.aliases.length != 1){
+                    $leftdiv.append("<li><a>" + charlist.name + " also known as " + charlist.aliases[0] +'</a></li>');
+                } else {
+                $leftdiv.append("<li><a>" + charlist.name +'</a></li>');
+                }
             }
         }
-        for (var i = 25; i < 500; i++){
+        for (var i = 26; i < 50; i++){
             var charlist = response[i];
             if (charlist.name != "" || charlist.name != undefined){
                 var $chardiv = $('#chardiv');
-                $chardiv.append("<li><a>" + charlist.name + '</a></li>');
-                
+                if (charlist.aliases.length != 1){
+                    $chardiv.append("<li><a>" + charlist.name + " also known as " + charlist.aliases[0] +'</a></li>');
+                } else {
+                    $chardiv.append("<li><a>" + charlist.name + '</a></li>');
+                }
             }
         }
-	}
+    }
+	
 
 	// handle XHR error
 	function updateUIError() {
@@ -43,5 +52,5 @@
 		var $weatherBox = $('#weather');
 		$weatherBox.addClass('hidden');
 	}
-
+    }
 })();
